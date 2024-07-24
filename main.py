eb5e3fd77050d8e989ae27be3260f8d38450064d
Nitiksh Kumar <122013711+nitikshh@users.noreply.github.com>
2024-07-24 10:47:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -181,4 +181,5 @@ def serve_clip(filename):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
